0512045615ad382b043e3f819f3fd4bd3bd75ebd
NikitaMashko <nikitamashko10@gmail.com>
2022-02-18 15:21:00 +0200

    first-commit

---
+++ src/components/v-catalog.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <h1>Catalog</h1>
+      <div class="v-catalog">
+        <v-catalog-item
+            v-for="product in PRODUCTS"
+            :key="product.article"
+            :product_data="product"
+            @sendArticle="showChildArticleInConsole"
+        />
+      </div>
+  </div>
+</template>
+
+<script>
+import vCatalogItem from './v-catalog-item'
+import {mapActions, mapGetters} from 'vuex'
+
+export default {
+  name: "v-catalog",
+  components: {
+    vCatalogItem
+  },
+  props: {},
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'PRODUCTS'
+    ]),
+  },
+  methods: {
+    ...mapActions([
+      'GET_PRODUCTS_FROM_API'
+    ]),
+    showChildArticleInConsole(data) {
+      console.log(data)
+    }
+  },
+  mounted() {
+    this.GET_PRODUCTS_FROM_API()
+    .then((response => {
+      if (response.data) {
+        console.log('Data arrived!')
+      }
+    }))
+  }
+}
+</script>
+
+<style>
+.v-catalog {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
